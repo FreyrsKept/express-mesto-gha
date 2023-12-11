aa186dbdafc67e4df57eb3cf6d7729cfc00578fa
FreyrsKept <120368406+FreyrsKept@users.noreply.github.com>
2023-12-11 18:18:14 +0700

    1.5
    fix app.js

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const mongoose = require('mongoose');
 const routeUsers = require('./routes/users');
 const routeCards = require('./routes/cards');
 const bodyParser = require('body-parser');
+const { ERROR_NOT_FOUND } = require('./utils/errors');
 const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+mongoose.set('strictQuery', true);
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use((req, res, next) => {
@@ -18,8 +21,12 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(routeCards);
-app.use(routeUsers);
+app.use('/users', routeUsers);
+app.use('/cards', routeCards);
+
+app.use((req, res) => {
+  res.status(ERROR_NOT_FOUND).send({ message: 'Страницы по запрошенному URL не существует' });
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
