20a611a64841a643b3eb9f48d6299c01e8d19cb0
FreyrsKept <120368406+FreyrsKept@users.noreply.github.com>
2024-01-08 17:20:08 +0700

    PR 14 2.7

--- errors/UnauthorizedError.js
+++ errors/UnauthorizedError.js
@@ -1,10 +1,7 @@
-const { UNAUTHORIZED_STATUS } = require('../config/config');
-
 class UnauthorizedError extends Error {
   constructor(message) {
     super(message);
-    this.statusCode = UNAUTHORIZED_STATUS;
+    this.statusCode = 401;
   }
 }
-
 module.exports = UnauthorizedError;
